ed547964fc3a460d4e824ff7064f4719b708b50f
skacholia <74026955+skacholia@users.noreply.github.com>
2023-04-26 17:24:20 -0400

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -9,25 +9,16 @@ st.set_page_config(
 )
 
 
-st.title("AthensAI: Democratizing AI for Local Candidates")
+st.title("The Earned Income Tax Credit (EITC)")
 
-st.markdown("""
-## The Problem
-Local Democratic candidates often face significant challenges when it comes to staffing and funding their campaigns. With limited resources, they struggle to effectively communicate with constituents, research opponents, navigate complex election finance laws, and refine their messaging.
+st.markdown(
+    """
+    # Earned Income Tax Credit (EITC)
 
-## Our Solution: AthensAI
-AthensAI offers a suite of powerful AI tools designed to help local Democratic candidates save time and money, allowing them to focus on what matters most - connecting with voters and winning elections.
-
-Here's what our app can do for you:
-
-### 1. Personalized Communication Tool
-**Why it's useful:** Crafting personalized communications, such as emails and press releases, can be time-consuming and challenging. Our AI tool generates tailored content that resonates with your target audience, streamlining your communication efforts.
-
-### 2. Opposition Research Tool
-**Why it's useful:** Researching opponents is a critical part of any campaign, but it can be difficult to stay updated on their activities. Our tool automatically gathers information from Google News and Twitter, keeping you informed about your opponents' actions and statements.
-
-### 3. FEC Chatbot
-**Why it's useful:** Navigating election finance laws can be complex and confusing. Our FEC Chatbot is here to help by answering your questions about election finance regulations, ensuring your campaign stays compliant and avoids
-
-### 4. Message Testing
-**Why it's useful:** Crafting the perfect message is essential for engaging voters and promoting your campaign. Our Message Testing Bot helps you refine your messaging by analyzing the impact and effectiveness of your content. With real-time feedback, you can optimize your communication strategy and ensure your message resonates with your target audience.""")
+    The **Earned Income Tax Credit (EITC)** is a refundable tax credit available to eligible taxpayers
+    in the United States. It is designed to help low-to-moderate income working individuals and families
+    by reducing their tax burden and potentially providing a refund. In many cases, the EITC can
+    significantly boost the income of those who qualify, making it an important tool in the fight
+    against poverty.
+    """
+)
