c6adbf08e563e1262aa3b12a6fff4a7b62d097f1
skacholia <74026955+skacholia@users.noreply.github.com>
2023-04-26 17:24:33 -0400

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -3,7 +3,6 @@ import streamlit.components.v1 as components
 
 st.set_page_config(
     page_title="Athens AI",
-    page_icon=":dart:",
     layout="wide",
     initial_sidebar_state="expanded",
 )
